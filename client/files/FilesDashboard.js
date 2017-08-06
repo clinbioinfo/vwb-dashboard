5be63e5d741e89a05185f994c2bad63626ec5a3a
Jaideep Sundaram <sundaram.medimmune@gmail.com>
2017-08-05 20:14:48 -0400

    Adds the following projection keys: basename': 1, 'path': 1, 'bytes_size': 1, 'owner': 1, 'uuid': 1

--- client/files/FilesDashboard.js
+++ client/files/FilesDashboard.js
@@ -10,7 +10,7 @@ Template.FilesDashboard.onCreated(function (){
 
 Template.FilesDashboard.helpers({
 	files: () => {
-		return Files.find({}, {sort : {createdAt: -1}});
+		return Files.find({}, {'basename': 1, 'path': 1, 'bytes_size': 1, 'owner': 1, 'uuid': 1}, {sort : {createdAt: -1}});
 	},
   	fileCount() {
     	return Files.find().count();
